91c63886366f7db4d24aa66cadce489fba7ec03d
Arthur PAILLE <a.paille@lyceesaintefamille.com>
2026-03-03 17:21:33 +0100

    TP15

--- monscript.js
+++ monscript.js
@@ -1,6 +1,11 @@
 /* Toutes les variables */
 let li = document.querySelector("#listeCourses li");
+let dateJour = new Date(Date.now());
+let dateJourFr = dateJour.toLocaleDateString('fr-FR')
+let title = document.querySelector("h2");
 
+title.textContent = `${title.textContent} du ${dateJourFr}`;
+            
 
 /* Tous les évènements */
 li.addEventListener('click', ()=>{
